cec482b5c7020669a9d90da256a7277fa69a5a65
Wadosh42 <None>
2024-12-03 17:25:46 +0000

    Copy configuration from gitlab

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -32,6 +32,7 @@ return {
         spell = false, -- sets vim.opt.spell
         signcolumn = "yes", -- sets vim.opt.signcolumn to yes
         wrap = false, -- sets vim.opt.wrap
+        swapfile = false, -- sets vim.opt.swapfile
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
@@ -62,10 +63,54 @@ return {
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
+        ["<Leader>b"] = { desc = "Buffers" },
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+
+        -- MNL custom section - START --
+
+        -- Switch between header/soucre
+        ["<leader>bo"] = {":ClangdSwitchSourceHeader<cr>", desc = "Switch to header/source"},
+
+        -- Toggle buffer explorer from Neotree
+        ["<leader>be"] = {":Neotree toggle buffers focus<cr>", desc = "Toggle Buffers Explorer Focus"},
+
+        -- Toggle git explorer from Neotree
+        ["<leader>ge"] = {":Neotree toggle git_status focus<cr>", desc = "Toggle Git Explorer Focus"},
+
+        -- Resize split equally
+        ["<leader>u="] = {":wincmd =<cr>", desc = "Resize split equally"},
+
+        -- Open telescope for symbol in file
+        ["<leader>fs"] = {":Telescope lsp_document_symbols<cr>", desc = "Find symbols in file"},
+
+        -- Open telescope for symbol in workspace
+        ["<leader>fS"] = {":Telescope lsp_workspace_symbols<cr>", desc = "Find symbols in workspace"},
+
+        -- Replace hexadecimal with decimal (and vice-versa)
+        -- Not really successfull implementation, keep only for remainder
+        -- ["<Leader>r"] = { desc = "Replace" },
+        -- ["<leader>rhd"] = {"viw:s/\\%V0x[[:xdigit:]]\\+/\\=str2nr(submatch(0), 16)<cr>", desc = "Replace hex with decimal"},
+        -- ["<leader>rdh"] = {"viw:s/\\%V[[:digit:]]\\+/\\=printf(\"0x%x\", submatch(0))<cr>", desc = "Replace decimal with hex"},
+        -- ["<leader>x"] = {"viw:s/\\(0x\\)\\?[[:digit:]]\\+/\\=submatch(0) =~ \"0x\" ? str2nr(submatch(0), 16) : printf(\"0x%x\", submatch(0))<cr>", desc = "Switch between hex/decimal"}
+
+        -- Replace hexadecimal with decimal (and vice-versa)
+        -- Successfull one: isolate expression on new line, substitute and return to initial position
+        ["<leader>x"] = {"lbi<cr> <Esc>:s/\\(0x\\)\\?[[:xdigit:]]\\+/\\=submatch(0) =~ \"0x\" ? str2nr(submatch(0), 16) : printf(\"0x%x\", submatch(0))<cr>0dwi<BS><Esc>l", desc = "Switch between hex/decimal"},
+
+        -- Move line up/down
+        ["<A-k>"] = {":m .-2<cr>==", desc = "Move line up"},
+        ["<A-j>"] = {":m .+1<cr>==", desc = "Move line down"},
+
+        -- MNL custom section - END --
+      },
+      t = {
+      },
+      v = {
+        -- MNL custom section - START --
+        ["<leader>-"] = {"y:g/<C-r>0/d<cr>", desc = "Delete line with selected pattern"},
+        -- MNL custom section - END --
       },
     },
   },
